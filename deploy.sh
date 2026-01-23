0f8f7c5cb9182f2e0ad48e8f6f97c1fb25b73b10
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2026-01-23 19:33:03 +0100

    provino script 2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git checkout main || { echo "❌ Errore: Checkout su main fallito. Fai il cazzo di commit prima."; exit 1; }
 git rm -rf .
 git checkout development -- build
-git checkout development -- deploy.sh
+git checkout development -- CNAME
 mv build/* .
 rmdir build
 git add .
