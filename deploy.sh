bcb70eb08cdaaa6a2e4c8e2a92796419086d226a
DavideGianessi <dgianessi03@gmail.com>
2026-01-23 23:21:47 +0100

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+[ $# -lt 1 ] && { echo "usage: ./deploy.sh \"{commit message}\""; exit 1; }
+npm run build || { echo "❌ npm build failed"; exit 1; }
+git add .
+git commit -m "$@"
 git checkout main || { echo "❌ Errore: Checkout su main fallito. Fai il cazzo di commit prima."; exit 1; }
 git rm -rf .
 git checkout development -- build
@@ -5,5 +9,5 @@ git checkout development -- CNAME
 mv build/* .
 rmdir build
 git add .
-git commit -m "Build: $(date)"
+git commit -m "Build: $@"
 git checkout development
