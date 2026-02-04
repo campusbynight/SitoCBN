e5cd525f65c17aa32435d745ac1eef5b268fdc98
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2026-02-04 15:10:41 +0100

    semplificazioe App.js logo animato definitivo

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ function App() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [loading, setLoading] = useState(true);
 
-  // Gestione del preloader: rimosso il timer fisso, ora controllato dalla fine del video
-  // useEffect(() => { ... }, []);
-
   const handleVideoUpdate = (e) => {
     // Fai partire l'apertura 0.5 secondi PRIMA della fine del video
     // Modifica '0.5' per anticipare o ritardare l'apertura
@@ -30,51 +27,12 @@ function App() {
   };
 
   useEffect(() => {
-    // Aggiorna l'indice attivo in base al percorso
-    switch (location.pathname) {
-      case '/':
-        setActiveIndex(0);
-        break;
-    //    case '/info':
-    //    setActiveIndex(1);
-    //    break;
-    //  case '/food':
-    //    setActiveIndex(2);
-    //    break;
-    //  case '/lottery':
-    //    setActiveIndex(3);
-    //    break;
-      case '/images':
-        setActiveIndex(4);
-        break;
-      default:
-        setActiveIndex(0);
-    }
     // Torna in cima alla pagina ad ogni cambio di rotta
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
   }, [location.pathname]);
 
   const handleSetActiveIndex = (index) => {
     setActiveIndex(index);
-    switch (index) {
-      case 0:
-        navigate('/');
-        break;
-    //  case 1:
-    //    navigate('/info');
-    //    break;
-    //  case 2:
-    //    navigate('/food');
-    //    break;
-    //  case 3:
-    //    navigate('/lottery');
-    //    break;
-      case 4:
-        navigate('/images');
-        break;
-      default:
-        navigate('/');
-    }
     // (opzionale) scroll animato quando si clicca il pulsante
     // window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   };
